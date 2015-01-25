5409caf8e9eacb5e6145c677c47cd1cd880dd746
Henry Kaufman <adventurerh@users.noreply.github.com>
2015-01-24 20:07:39 -0700

    trying to build a distance away from you

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,6 @@ angular.module('starter.controllers', ['firebase'])
     $scope.userThreads = sync.$asArray();
 
     var pos;
-    //fix later, doesn't work yet
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -33,23 +32,64 @@ angular.module('starter.controllers', ['firebase'])
         }
     }
 
-    $scope.add = function (post) {
+    $scope.add = function (thread) {
         $scope.userThreads.$add({
             user: $scope.user,
-            title: post.title,
-            desc: post.desc,
+            title: thread.title,
+            desc: thread.desc,
             location: pos,
             date: 0,
             category: 'Soon to come'
         });
 
-        post.title = "";
-        post.desc = "";
+        thread.title = "";
+        thread.desc = "";
     }
 })
 
-.controller('ChatsCtrl', function ($scope, Chats) {
-    
+.controller('ChatsCtrl', function ($scope, $firebase, fireBaseData) {
+    var ref = new Firebase("https://threadstsa.firebaseio.com/nearbyThreads/");
+    var sync = $firebase(ref);
+
+    $scope.user = fireBaseData.ref().getAuth();
+
+    $scope.nearbyThreads = sync.$asArray();
+
+    var pos;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            pos = new google.maps.LatLng(position.coords.latitude,
+                position.coords.longitude);
+        }, function () {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
+
+    function handleNoGeolocation(errorFlag) {
+        if (errorFlag) {
+            var content = 'Error: The Geolocation service failed.';
+        } else {
+            var content = 'Error: Your browser doesn\'t support geolocation.';
+        }
+    }
+
+    $scope.add = function (thread) {
+        $scope.nearbyThreads.$add({
+            user: $scope.user,
+            title: thread.title,
+            desc: thread.desc,
+            location: pos,
+            date: 0,
+            category: 'Soon to come'
+        });
+        
+        thread.title = "";
+        thread.desc = "";
+    }
 })
 
 .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
