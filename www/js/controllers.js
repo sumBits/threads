1d37752b3c61b495e859439c36a8a75eed158711
Henry Kaufman <adventurerh@users.noreply.github.com>
2015-01-24 18:31:20 -0700

    FIXED GEOLOCATION

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,39 +5,41 @@ angular.module('starter.controllers', ['firebase'])
 .controller('DashCtrl', function ($scope, $firebase, fireBaseData) {
     var ref = new Firebase("https://threadstsa.firebaseio.com/feeds/");
     var sync = $firebase(ref);
-    
+
     $scope.user = fireBaseData.ref().getAuth();
-    
+
     $scope.feeds = sync.$asArray();
 
-    $scope.add = function (post) {
-        //fix later, doesn't work yet
-        /*
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    var pos = new google.maps.LatLng(position.coords.latitude,
-                        position.coords.longitude);
-                }, function () {
-                    handleNoGeolocation(true);
-                });
-            } else {
-                // Browser doesn't support Geolocation
-                handleNoGeolocation(false);
-            }
-        }
+    var pos;
+    //fix later, doesn't work yet
 
-        function handleNoGeolocation(errorFlag) {
-            if (errorFlag) {
-                var content = 'Error: The Geolocation service failed.';
-            } else {
-                var content = 'Error: Your browser doesn\'t support geolocation.';
-            }
-        }*/
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            pos = new google.maps.LatLng(position.coords.latitude,
+                position.coords.longitude);
+        }, function () {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
+
+    function handleNoGeolocation(errorFlag) {
+        if (errorFlag) {
+            var content = 'Error: The Geolocation service failed.';
+        } else {
+            var content = 'Error: Your browser doesn\'t support geolocation.';
+        }
+    }
+
+    $scope.add = function (post) {
+
         $scope.feeds.$add({
             user: $scope.user,
             title: post.title,
             desc: post.desc,
-            location: 0,
+            location: pos,
             date: 0,
             category: 'Soon to come'
         });
@@ -64,21 +66,21 @@ angular.module('starter.controllers', ['firebase'])
 })
 
 .controller('AccountCtrl', function ($scope, fireBaseData) {
-    $scope.showLoginForm = false; 
+    $scope.showLoginForm = false;
     $scope.user = fireBaseData.ref().getAuth();
     if (!$scope.user) {
         $scope.showLoginForm = true; //checks if the user has logged in; if true, the user is not logged in and the login form will be displayed
     }
-    
+
     //Login method
-    $scope.login = function(em, pwd){
+    $scope.login = function (em, pwd) {
         console.log(em);
         console.log(pwd);
         fireBaseData.ref().authWithPassword({
             email: em,
             password: pwd
-        }, function(error, authData) {
-            if(error === null){
+        }, function (error, authData) {
+            if (error === null) {
                 console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
                 $scope.user = fireBaseData.ref().getAuth();
                 $scope.showLoginForm = false;
@@ -88,11 +90,11 @@ angular.module('starter.controllers', ['firebase'])
             }
         });
     };
-    
+
     //Logout method
-    $scope.logout = function(){
+    $scope.logout = function () {
         fireBaseData.ref().unauth();
         $scope.showLoginForm = true;
     };
-    
+
 });
