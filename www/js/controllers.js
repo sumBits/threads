b91de1299de7a5b8f25df6db5348b4b8218d70d4
whitedeath820 <allensam0@gmail.com>
2015-01-27 11:59:49 -0700

    added chat to local threads

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -174,8 +174,19 @@ angular.module('starter.controllers', ['firebase', 'ngCordova'])
 
 })
 
-.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-    $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams) {
+    var a = $stateParams.threadId;
+    console.log(a);
+    var ref = new Firebase("https://threadstsa.firebaseio.com/nearbyThreads/" + a + "/comments/");
+    var sync = $firebase(ref);
+    $scope.comments = sync.$asArray();
+
+    $scope.submitPost = function (post) {
+        $scope.comments.$add({
+            user: $scope.user.password.email,
+            message: post.content
+        });
+    };
 })
 
 .controller('FriendsCtrl', function ($scope, Friends) {
